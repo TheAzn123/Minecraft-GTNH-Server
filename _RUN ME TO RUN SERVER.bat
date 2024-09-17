eddab2101babc43d6eb4a56b76d212d6bf9da7a8
TheAzn123 <Jdobedicen@gmail.com>
2024-09-17 01:06:20 -0700

    Auto-commit after server stop

--- _RUN ME TO RUN SERVER.bat
+++ _RUN ME TO RUN SERVER.bat
@@ -1,29 +1,17 @@
+@echo off
 ::When server is ran, cd to current directory and disk
 cd /d "%~dp0"
 
 ::Pulls from current repository
 echo Pulling from main
 git pull origin main
-
+echo ================================
+echo INPUT stop TO STOP SERVER AND COMMIT TO REPO. YOU MUST DO THIS
+echo ================================
+pause
 ::Run server
 call startserver-java9.bat
 
-echo
-echo ==========================================================
-echo Type exit to stop the server
-echo ==========================================================
-set /p exitcommand=Type 'exit' to stop the server:
-
-if "%exitcommand%" == "exit" (
-    echo Stopping Minecraft server...
-    echo stop | java -Xmx4G -Xms1G -jar server.jar nogui
-    goto end
-) else (
-    set /p exitcommand=Type 'exit' to stop the server:
-    goto loop
-)
-::Commit and pushd
-:end
 echo ==========================================================
 echo WAIT FOR COMMIT AND PUSH BEFORE CLOSING WINDOW OR YOU BREAK THINGS
 echo ==========================================================
